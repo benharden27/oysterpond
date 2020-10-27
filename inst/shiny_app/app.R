50488da1feae4674ab0521aba2eb40968e4b017c
benharden27 <benharden27@gmail.com>
2020-10-26 20:20:33 -0400

    adjustments to plotting and adding a date range

--- inst/shiny_app/app.R
+++ inst/shiny_app/app.R
@@ -13,6 +13,7 @@ library(leaflet)
 library(shinydashboard)
 library(oysterpond)
 
+data(op_dat)
 site_locs <- op_dat[!duplicated(op_dat$site),c(2,28,29)]
 op_dat$depth = as.character(op_dat$depth)
 op_dat <- select(op_dat,date,site,depth,temp,salinity,do_pc,tdn,tdp,chl)
@@ -24,7 +25,7 @@ op_meta_dat <- tibble::tibble(var = names(op_dat),
                                        "Nitrogen",
                                        "Phosophorus",
                                        "Chlorophyll-a"),
-                              unit = c("","","m","^oC","ppt","%","mg/L",
+                              unit = c("","","m","˚C","ppt","%","mg/L",
                                        "ug/L","ug/L"))
 
 
@@ -35,23 +36,23 @@ ui <- dashboardPage(
    dashboardHeader(title = "Oyster Pond Data Explorer"),
 
    # Sidebar with a slider input for number of bins
-   dashboardSidebar(
-     fluidPage(h3(textOutput("site"))),
+   dashboardSidebar(width = 200,
+     fluidPage(h3("Site:",textOutput("site"))),
      uiOutput("vars"),
      uiOutput("depths"),
-     checkboxGroupInput("lt","Choose how to plot",
+     sliderInput("dates","Date Range",
+                 as.Date(min(op_dat$date,na.rm=T)),as.Date(max(op_dat$date,na.rm=T)),
+                 c(as.Date(min(op_dat$date,na.rm=T)),as.Date(max(op_dat$date,na.rm=T)))),
+     checkboxGroupInput("lt","Plot Style",
                         choices = c("Points","Lines","Smoothed"),
                         selected = "Points")
    ),
 
    dashboardBody(
      fluidRow(
-       box(leafletOutput("map"), width = 12)
-     ),
-     fluidRow(
-       box(plotOutput("series"), width = 12)
-     )
-   )
+       box(leafletOutput("map"),width = 4, title = "Region Map"),
+       box(plotOutput("series"), width = 8, title = "Data Plotter")
+     ))
 )
 
 # Define server logic required to draw a histogram
@@ -66,15 +67,49 @@ server <- function(input, output) {
     }
   }, ignoreNULL = FALSE)
 
-  observe({
-    proxy <- leafletProxy('map')
-    proxy %>%
-      removeMarker(layerId = "test") %>%
-      addCircleMarkers(layerId = "test", data = dplyr::filter(site_locs,site == site()), color = "red")
-  })
-
   output$site <- renderText({
     site()
+  })
+
+  output$vars <- renderUI({
+    selectInput("var","Variable",
+                choices = find_notna(op_dat,op_meta_dat,site()),
+                selected = "temp")
+  })
+
+  output$depths <- renderUI({
+    data <- op_dat[op_dat$site==site(), ]
+    checkboxGroupInput("depth","Depths (m)",unique(data$depth),selected = "0")
+  })
+
+  output$series <- renderPlot({
+    data <- op_dat[op_dat$site == site(), ]
+    data <- subset(data, depth %in% input$depth)
+    g <- ggplot(data)
+    if("Points" %in% input$lt) {
+      g <- g + geom_point(aes_string(x="date",y=input$var,color="depth"),
+                          na.rm = TRUE, size = 2)
+    }
+    if("Lines" %in% input$lt) {
+      g <- g + geom_line(aes_string(x="date",y=input$var,color="depth"),
+                         na.rm = TRUE)
+    }
+    if("Smoothed" %in% input$lt) {
+      g <- g + geom_smooth(aes_string(x="date",y=input$var,color="depth"),
+                           na.rm = TRUE)
+    }
+    g +
+      coord_cartesian(xlim = as.POSIXct(input$dates)) +
+      labs(x = NULL,
+           y = paste0(filter(op_meta_dat,var == input$var)$name," (",
+                      filter(op_meta_dat,var == input$var)$unit, ")"),
+           color = "Depth (m)") +
+      theme_bw() +
+      theme(panel.grid.minor = element_blank(),
+            panel.border = element_blank(),
+            axis.ticks = element_blank())
+
+
   })
 
   output$map <- renderLeaflet({
@@ -85,37 +120,12 @@ server <- function(input, output) {
       addCircleMarkers(label = ~site)
   })
 
-  output$series <- renderPlot({
-    data <- op_dat[op_dat$site == site(), ]
-    data <- subset(data, depth %in% input$depth)
-    g <- ggplot(data)
-    if("Points" %in% input$lt) {
-      g <- g + geom_point(aes_string(x="date",y=input$var,color="depth"),
-                 na.rm = TRUE)
-    }
-    if("Lines" %in% input$lt) {
-      g <- g + geom_line(aes_string(x="date",y=input$var,color="depth"),
-                 na.rm = TRUE)
-    }
-    if("Smoothed" %in% input$lt) {
-      g <- g + geom_smooth(aes_string(x="date",y=input$var,color="depth"),
-                 na.rm = TRUE)
-    }
-    g
-
+  observe({
+    proxy <- leafletProxy('map')
+    proxy %>%
+      removeMarker(layerId = "test") %>%
+      addCircleMarkers(layerId = "test", data = dplyr::filter(site_locs,site == site()), color = "red")
   })
-
-  output$vars <- renderUI({
-    selectInput("var","Choose a variable",
-                choices = find_notna(op_dat,op_meta_dat,site()),
-                selected = "temp")
-  })
-
-  output$depths <- renderUI({
-    data <- op_dat[op_dat$site==site(), ]
-    checkboxGroupInput("depth","Select depths (m)",unique(data$depth),selected = "0")
-  })
-
 }
 
 # Run the application
